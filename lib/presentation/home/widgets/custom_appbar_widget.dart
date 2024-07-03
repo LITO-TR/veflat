7477fca169d219d89e92ed85bfa9adadfff2014f
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2024-07-02 19:50:01 -0500

    feat: added counter to cart

--- lib/presentation/home/widgets/custom_appbar_widget.dart
+++ lib/presentation/home/widgets/custom_appbar_widget.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:veflat/presentation/cart/screen/cart_screen.dart';
+import 'package:veflat/presentation/product/providers/product_provider.dart';
 
-class CustomAppBarWidget extends StatelessWidget
-    implements PreferredSizeWidget {
+class CustomAppBarWidget extends ConsumerWidget implements PreferredSizeWidget {
   const CustomAppBarWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final products = ref.watch(productProvider);
+
     return AppBar(
       elevation: 0,
       leading: IconButton(
@@ -27,16 +30,41 @@ class CustomAppBarWidget extends StatelessWidget
           ),
           onPressed: () {},
         ),
-        IconButton(
-          splashRadius: 20,
-          icon: const Icon(
-            FontAwesomeIcons.bagShopping,
-            color: Colors.black,
-          ),
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const CartScreen()));
-          },
+        Stack(
+          children: [
+            IconButton(
+              splashRadius: 20,
+              icon: const Icon(
+                FontAwesomeIcons.bagShopping,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const CartScreen()));
+              },
+            ),
+            if (products.cartProducts!.isNotEmpty)
+              Positioned(
+                right: 0,
+                child: Container(
+                  padding: const EdgeInsets.all(6),
+                  decoration: const BoxDecoration(
+                    color: Colors.orange,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Text(
+                    products.cartProducts!.length.toString(),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+          ],
         ),
       ],
     );
